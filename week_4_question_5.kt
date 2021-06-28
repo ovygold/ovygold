cbbada04e49583c91ab6fc8bcc7f3a8ac2ec8424
ovygold <85237526+ovygold@users.noreply.github.com>
2021-06-28 18:29:33 +0100

    Add files via upload

---
+++ week_4_question_5.kt
@@ -0,0 +1,12 @@
+
+fun main(args: Array<String>) {
+
+ spaced (2, 4, 6) 
+
+}
+fun spaced(a: Int, b: Int, c:Int) { var d: Boolean = true
+         var g: Boolean = false       
+         if ((a-b)==(b-c))
+            {println(d)}
+         else { println(g) }
+}
